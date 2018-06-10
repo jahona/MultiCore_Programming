2094851076f7e17f81368aac379c34bb4aaa08d8
jahona <jahona5240@gmail.com>
2018-06-11 03:08:48 +0900

    apply padding

--- bitonic_sort/main.cu
+++ bitonic_sort/main.cu
@@ -4,30 +4,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include "DS_timer.h"
+#include <math.h>
 
 void BitonicSort(int, int, bool);
 void BitonicMerge(int, int, bool);
 void print();
+bool CheckPowOfTwo(int);
 
 int * arr;
 int * origin_arr;
-int MAX;
+int arr_length;
 
 int main(int argc, char * argv[]) {
-	MAX = atoi(argv[1]);
+	arr_length = atoi(argv[1]);
+	int padding = 1;
+	int temp;
 
-	arr = new int[MAX];
-	origin_arr = new int [MAX];
+	if(!CheckPowOfTwo(arr_length)) {
+		temp = arr_length;
 
-	for(int i=0 ; i<MAX ; i++) {
+		while(true) {
+			temp = temp >> 1;
+			padding = padding << 1;
+			if(temp == 0) break;
+		}
+
+		padding -= arr_length;					
+	}
+	
+	arr = new int[arr_length + padding];
+	origin_arr = new int [arr_length + padding];
+
+	for(int i=0 ; i<arr_length ; i++) {
 		arr[i] = rand() % 200;
 		origin_arr[i] = arr[i];		
 	}
 
+	for(int i=0 ; i<padding ; i++) {
+		arr[arr_length + i] = INT_MAX;
+		origin_arr[arr_length + i] = INT_MAX;
+	}
+	
 	print();
-
-	BitonicSort(0, MAX, true);
-
+		
+	BitonicSort(0, arr_length+padding, true);
+	
 	print();
 }
 
@@ -62,7 +83,7 @@ void BitonicMerge(int start, int n, bool isAscending) {
 }
 
 void print() {
-	for(int i=0 ; i<MAX ; i++) {
+	for(int i=0 ; i<arr_length ; i++) {
 		if(origin_arr[i] != arr[i]) {
 			printf("%d*\t", arr[i]);
 			origin_arr[i] = arr[i];
@@ -73,3 +94,11 @@ void print() {
 	printf("\n");
 }
 	
+bool CheckPowOfTwo(int num) {
+	int result = num & (num - 1);
+
+	if(result == 0)
+		return true;
+	else
+		return false;
+}
